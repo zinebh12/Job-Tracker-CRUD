7649f5071c4d8a1a20ffc0d9d6d3ff9a8025e3a7
Zineb <krehtyazeh@gmail.com>
2026-09-03 22:04:53 +0100

    POST REQ DONE

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,11 +6,27 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//GET
 app.get("/api/applications", async (req, res) => {
   try {
     const applications = await pool.query("SELECT * FROM applications");
     res.json(applications.rows);
     console.log(applications.rows);
+  } catch (err: any) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
+//POST
+app.post("/api/applications", async (req, res) => {
+  try {
+    const { company, position, location, status } = req.body;
+    const newApplication = await pool.query(
+      "INSERT INTO applications (company, position,location, status) VALUES ($1, $2, $3, $4) RETURNING *",
+      [company, position, location, status],
+    );
+    res.json(newApplication.rows[0]);
   } catch (err: any) {
     console.error(err.message);
     res.status(500).send("Server Error");
