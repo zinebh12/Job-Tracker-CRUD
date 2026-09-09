a7178669c5b47b8d8990799a417ec707c49647d0
Zineb <krehtyazeh@gmail.com>
2026-09-09 17:36:54 +0100

    added authAPI for frontend

---
+++ frontend/src/services/authApi.ts
@@ -0,0 +1,51 @@
+import type { RegisterData, LoginData } from "@/types/auth";
+const API_BASE_URL = "http://localhost:5000/api/auth";
+
+export const register = async (user: RegisterData) => {
+  const response = await fetch(`${API_BASE_URL}/register`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(user),
+  });
+  const data = await response.json();
+  if (!response.ok) {
+    throw new Error(data.error || "Failed to register user");
+  }
+  return data;
+};
+
+export const login = async (user: LoginData) => {
+  const response = await fetch(`${API_BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+    body: JSON.stringify(user),
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error || "Invalid email or password");
+  }
+
+  return data;
+};
+
+export const logout = async () => {
+  const response = await fetch(`${API_BASE_URL}/logout`, {
+    method: "POST",
+    credentials: "include",
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error || "Failed to logout");
+  }
+
+  return data;
+};
---
+++ frontend/src/types/auth.ts
@@ -0,0 +1,17 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface RegisterData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginData {
+  email: string;
+  password: string;
+}
+
