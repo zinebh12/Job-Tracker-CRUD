d66ec83598cd1bed466515df5aed205cf2474ff0
Zineb <krehtyazeh@gmail.com>
2026-09-10 18:57:57 +0100

    Update authController.ts

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -76,7 +76,7 @@ export const login = async (req: Request, res: Response) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return res.json({
@@ -93,7 +93,7 @@ export const logout = (req: Request, res: Response) => {
   res.cookie("token", {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
   });
 
   return res.json({
