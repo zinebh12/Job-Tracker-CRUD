f2f24bab4e5a6a89486a3d434c40957edd0c3851
Zineb <krehtyazeh@gmail.com>
2026-09-10 17:39:42 +0100

    production ready

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import applicationRoutes from "./routes/applicationRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
 const app = express();
+
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
