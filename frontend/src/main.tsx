69eea750c2378d323d242acfbf50c99789f0a0ac
Zineb <krehtyazeh@gmail.com>
2026-09-10 17:29:27 +0100

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
-import { AuthProvider } from "@/context/authContext.tsx";
+import { AuthProvider } from "@/context/AuthContext.tsx";
 import "./index.css";
 import App from "./App.tsx";
 
