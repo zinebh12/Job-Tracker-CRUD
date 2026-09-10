a5b5f21aa7c46b3f492930a46b673eea2d97ab10
Zineb <krehtyazeh@gmail.com>
2026-09-10 17:05:37 +0100

    preparing for production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,10 @@
 import "dotenv/config";
 import express from "express";
-import cors from "cors";
 import applicationRoutes from "./routes/applicationRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
 import cookieParser from "cookie-parser";
 const app = express();
-
-// app.use(cors());
-
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  }),
-);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,7 +14,7 @@ app.use("/api/auth", authRoutes);
 
 app.use(errorHandler);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- frontend/src/services/applicationApi.ts
+++ frontend/src/services/applicationApi.ts
@@ -4,7 +4,7 @@ import type {
   UpdateApplication,
 } from "./../types/applications";
 
-const API_BASE_URL = `${import.meta.env.VITE_API_URL}/applications`;
+const API_BASE_URL = `/api/applications`;
 
 export const getApplications = async (
   page = 1,
